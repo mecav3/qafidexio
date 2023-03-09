91dd6dc8464cf13fa19514ee5aadc12fcfdce1bf
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-03-10 02:05:31 +0300

    done 3 cases

---
+++ src/test/java/org/step_definitions/Hooks.java
@@ -0,0 +1,37 @@
+package org.step_definitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.util.Driver;
+
+public class Hooks {
+
+//    @Before(order = 2)
+//    public static void setupScenario() {
+//        System.out.println("setting things @Before");
+//    }
+
+    @After
+    public void teardownscenario(Scenario scenario) {
+        System.out.println("---closing browser @After");
+
+     if (scenario.isFailed()) {
+         byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+         scenario.attach(screenshot, "image/png", scenario.getName());
+     }
+
+    //   Driver.closeDriver();
+    }
+//
+//    @BeforeStep
+//    public void setup() {
+//        System.out.println("before step setup @BeforeStep");
+//    }
+//
+//    @AfterStep
+//    public void aftersetup() {
+//        System.out.println("before step setup @AfterStep");
+//    }
+ }
